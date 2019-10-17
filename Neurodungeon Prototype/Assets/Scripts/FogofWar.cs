44d823a2120d4c80e1612c9da090a0f9233f5839
Claudio-Visentin <D16126698@mydit.ie>
2019-10-17 14:26:48 +0100

    Added simple fog of war

---
+++ Neurodungeon Prototype/Assets/Scripts/FogofWar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogofWar : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag ("Player"))
+        {
+            Score.score += 5;
+            Destroy(gameObject);
+        }
+    }
+}
