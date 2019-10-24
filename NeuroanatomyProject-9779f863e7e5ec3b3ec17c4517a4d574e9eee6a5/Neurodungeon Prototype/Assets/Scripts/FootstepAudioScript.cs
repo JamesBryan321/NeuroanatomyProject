292504ba4ab8adac9b67e5e1a18158c937de8212
Finnchris13 <finnchris13@gmail.com>
2019-10-24 18:17:26 +0100

    Adding sound and scripts
    Added footstep sound effects, torch sound effects, and torches lighting when triggered

---
+++ NeuroanatomyProject-9779f863e7e5ec3b3ec17c4517a4d574e9eee6a5/Neurodungeon Prototype/Assets/Scripts/FootstepAudioScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepAudioScript : MonoBehaviour
+{
+
+    public AudioClip footstep;
+    public AudioClip footsteptwo;
+
+    public AudioSource myAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FootStep()
+    {
+
+        myAudio.PlayOneShot(footstep);
+
+    }
+
+    public void FootStepTwo()
+    {
+
+        myAudio.PlayOneShot(footsteptwo);
+
+    }
+
+}
---
+++ NeuroanatomyProject-9779f863e7e5ec3b3ec17c4517a4d574e9eee6a5/Neurodungeon Prototype/Assets/Scripts/TorchScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchScript : MonoBehaviour
+{
+
+    public AudioClip TorchFadeIn;
+    public AudioClip TorchLoop;
+
+    public GameObject LightSourceOne;
+    public GameObject LightSourceTwo;
+
+    AudioSource myAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myAudio = GetComponent<AudioSource>();
+        myAudio.clip = TorchFadeIn;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.tag == "Player")
+        {
+
+            myAudio.Play();
+            Invoke("TorchLooping", 15.3f);
+            LightSourceOne.SetActive(true);
+            LightSourceTwo.SetActive(true);
+            GetComponent<Collider>().enabled = false;
+
+        }
+
+    }
+
+    void TorchLooping()
+    {
+
+        myAudio.clip = TorchLoop;
+        myAudio.Play();
+
+    }
+
+}
