cbf9e8ed6535ecbaeb84465e014b47c75ec95565
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2019-10-25 09:32:13 +0100

    Interactable Objects Functional & Questions Updated
    Updated question script to support different questions on different objects.
    Interactable objects now work also.
    Added Animation functionality for correct answers and free the prisoner.

---
+++ NeuroanatomyProject-9779f863e7e5ec3b3ec17c4517a4d574e9eee6a5/Neurodungeon Prototype/Assets/Scripts/QuestionIndexer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionIndexer : MonoBehaviour
+{
+    public int questionIndex;
+}
---
+++ NeuroanatomyProject-9779f863e7e5ec3b3ec17c4517a4d574e9eee6a5/Neurodungeon Prototype/Assets/Scripts/clickObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickObject : MonoBehaviour
+{
+    public Material hitMaterial;
+
+    public ButtonScript bS;
+    public QuestionIndexer qIndexScript;
+    
+    // Use this for initialization
+    void Start () {
+		
+    }
+	
+    // Update is called once per frame
+    void Update () 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                var rig = hitInfo.collider;
+                if(rig.CompareTag("Interactable"))
+                {
+                    rig.GetComponent<MeshRenderer>().material = hitMaterial;
+                    qIndexScript = rig.GetComponent<QuestionIndexer>();
+                    Debug.Log(rig.name);
+                    bS.OpenQuestion1(qIndexScript.questionIndex);
+                }
+            }
+        }
+
+    }
+}
