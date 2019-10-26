4fc7b6bad51af4bfa340f1bcafef7c9c0e7b3d9f
Claudio-Visentin <D16126698@mydit.ie>
2019-10-26 10:49:17 +0100

    Revert "Interactable Objects Functional & Questions Updated"
    This reverts commit cbf9e8ed6535ecbaeb84465e014b47c75ec95565.

--- NeuroanatomyProject-9779f863e7e5ec3b3ec17c4517a4d574e9eee6a5/Neurodungeon Prototype/Assets/Scripts/ButtonScript.cs
+++ NeuroanatomyProject-9779f863e7e5ec3b3ec17c4517a4d574e9eee6a5/Neurodungeon Prototype/Assets/Scripts/ButtonScript.cs
@@ -2,87 +2,34 @@
 
 public class ButtonScript : MonoBehaviour
 {
-    public Canvas menuCanvas;
     public Canvas question1Canvas;
-    public Canvas question2Canvas;
-
-    public InGameUI uIScript;
 
     public AudioClip journalOpening;
     public AudioClip journalClosing;
-
-    public GameObject prisoner1;
-    public Animator p1Animator;
-
-    public GameObject prisonCell1;
-    public Animator prisonCell1Animator;
 
     AudioSource myAudio;
 
     private void Start()
     {
         question1Canvas.enabled = false;
-        question2Canvas.enabled = false;
-
-        p1Animator = prisoner1.GetComponent<Animator>();
-        prisonCell1Animator = prisonCell1.GetComponent<Animator>();
-        
         myAudio = GetComponent<AudioSource>();
         
     }
-    
-    public void OpenMenu()
-    {
-       
-            if (menuCanvas.enabled == false)
-            {
-                menuCanvas.enabled = true;
-                uIScript.OnMapButton();
-                myAudio.clip = journalOpening;
-                myAudio.Play();
-            }
-            else if (menuCanvas.enabled == true)
-            {
-                menuCanvas.enabled = false;
-                myAudio.clip = journalClosing;
-                myAudio.Play();
-            }
-    }
 
-    public void OpenQuestion1(int questionIndex)
+    public void OpenQuestion()
     {
-        if (questionIndex == 1)
+        //Handles the quest book opening and closing
+        if (question1Canvas.enabled == false)
         {
-            if (question1Canvas.enabled == false)
-            {
-                question1Canvas.enabled = true;
-                myAudio.clip = journalOpening;
-                myAudio.Play();
-            }
-            else if (question1Canvas.enabled == true)
-            {
-                question1Canvas.enabled = false;
-                myAudio.clip = journalClosing;
-                myAudio.Play();
-            }
+            question1Canvas.enabled = true;
+            myAudio.clip = journalOpening;
+            myAudio.Play();
         }
-        
-        if (questionIndex == 2)
+        else if (question1Canvas.enabled == true)
         {
-            if (question2Canvas.enabled == false)
-            {
-                question2Canvas.enabled = true;
-                myAudio.clip = journalOpening;
-                myAudio.Play();
-            }
-            else if (question2Canvas.enabled == true)
-            {
-                question2Canvas.enabled = false;
-                myAudio.clip = journalClosing;
-                p1Animator.SetBool("Correct", true);
-                prisonCell1Animator.SetBool("Correct", true);
-                myAudio.Play();
-            }
+            question1Canvas.enabled = false;
+            myAudio.clip = journalClosing;
+            myAudio.Play();
         }
     }
 }
