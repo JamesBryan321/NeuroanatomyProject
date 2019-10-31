5d530f929b67c141cb72760be650e4cf9b02ede3
Claudio-Visentin <D16126698@mydit.ie>
2019-10-31 16:05:24 +0000

    Added area complete reward
    Added area complete reward + minor fixes

---
+++ NeuroanatomyProject1/NeuroanatomyProject2/Neurodungeon Prototype/Assets/Scripts/AreaComplete.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaComplete : MonoBehaviour
+{
+    public List<GameObject> fogs = new List<GameObject>();
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Fog")
+        {
+            fogs.Add(other.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        fogs.RemoveAll(GameObject => GameObject == null);
+        if (fogs.Count == 0)
+        {
+            Score.score += 200;
+            //Chris add sfx here
+            Destroy(this);
+        }
+    }
+}
