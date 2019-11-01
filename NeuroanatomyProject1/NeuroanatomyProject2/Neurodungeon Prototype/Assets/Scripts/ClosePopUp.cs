fd3b9b0be4c752e7943876d12253e79b187962ae
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2019-11-01 02:11:11 +0000

    PopUps Started
    Made a start on adding popup tips, in-progress

---
+++ NeuroanatomyProject1/NeuroanatomyProject2/Neurodungeon Prototype/Assets/Scripts/ClosePopUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClosePopUp : MonoBehaviour
+{
+    //public GameObject thisPopUp;
+    
+    public void CloseWindow()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
