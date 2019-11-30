4b7e208f18f07e390d024782dafe5b1e63584dfb
Claudio-Visentin <D16126698@mydit.ie>
2019-11-30 14:56:50 +0000

    Created working prefab of final test, need to code questions + added barrels at start

---
+++ NeuroanatomyProject1/NeuroanatomyProject2/Neurodungeon Prototype/Assets/Scripts/FinalTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalTest : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject finaltestgameobject;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("Player"))
+        {
+            anim.SetBool("close", true);
+            finaltestgameobject.tag = "Interactable";
+            finaltestgameobject.layer = 8;
+        }
+    }
+
+    public void PassedFinalTest ()
+    {
+        anim.SetBool("close", false);
+        finaltestgameobject.tag = "notInteractable";
+    }
+}
